44ae958b821bc89c34d3e89531793babe83aabe4
Aditya sharma <sharmaaditya6785@gmail.com>
2022-07-31 10:01:43 +0530

    level 3 security using the hashing

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const express= require("express")
 const bodyParser = require("body-parser")
 const ejs = require("ejs")
 const mongoose = require("mongoose")
-const encrypt = require("mongoose-encryption")
+const md5 = require("md5")
 
 
 const app = express()
@@ -28,7 +28,7 @@ const userSchema = new mongoose.Schema({
 
 // level 2 security encrypting the password 
 
-userSchema.plugin(encrypt,{secret: process.env.SECRET, encryptedFields:["password"]});
+
 
 // creating the model 
 
@@ -48,7 +48,7 @@ app.get("/register",function(req,res){
 app.post("/register",function(req,res){
     const newuser = new User({
         email: req.body.username,
-        password: req.body.password
+        password: md5(req.body.password)
     })
     newuser.save(function(err){
         if(err){
@@ -63,7 +63,7 @@ app.post("/register",function(req,res){
 app.post("/login",function(req, res){
     
     const username = req.body.username 
-    const password= req.body.password
+    const password= md5(req.body.password)
     User.findOne({email: username}, function(err,foundUser){
         if(err){
             console.log(err)
